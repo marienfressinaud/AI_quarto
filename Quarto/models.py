e337b75386dd23d258bcc85c3fe884fc78585bac
Marien Fressinaud <dev@marienfressinaud.fr>
2013-08-23 16:26:22 +0200

    Player added
    Player has been added
    selectPiece() and putOnBoard() are not yet implemented

--- Quarto/models.py
+++ Quarto/models.py
@@ -17,3 +17,28 @@ class Piece:
 		self.size = properties['size']
 		self.shape = properties['shape']
 		self.consistency = properties['consistency']
+
+
+class Player:
+	'''
+	A Player is defined by:
+	* a name
+	* an intelligence (AI or not)
+	'''
+
+	def __init__(self, name, intelligence):
+		self.name = name
+		self.intelligence = intelligence
+
+		if(self.intelligence is None):
+			self.isAI = False
+		else:
+			self.isAI = True
+
+	def selectPiece():
+		print "Not yet implemented"
+		return None
+
+	def putOnBoard():
+		print "Not yet implemented"
+		return None
--- Quarto/quarto.py
+++ Quarto/quarto.py
@@ -2,16 +2,25 @@
 # -*- coding: utf-8 -*-
 
 from models import Piece
+from models import Player
 
 def main():
-	# properties = {
-	# 	'color': 'blue',
-	# 	'size': 'small',
-	# 	'shape': 'round',
-	# 	'consistency': 'hole'
-	# }
-	# piece = Piece(properties)
-	# print piece.color
+	properties = {
+		'color': 'blue',
+		'size': 'small',
+		'shape': 'round',
+		'consistency': 'hole'
+	}
+	piece = Piece(properties)
+	print piece.color
+
+	player = Player("Patrick", None)
+	print player.name
+	if player.isAI:
+		print "Player has an artificial intelligence"
+	else:
+		print "Player is a human"
+
 	print "Not implemented"
 
 if __name__ == "__main__":
